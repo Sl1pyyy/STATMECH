d19efd326b500998c1a6bbf2b18f6ae39b80f4d1
PUDELEK <prometey202020@gmail.com>
2026-03-29 09:29:23 +0200

    pbnum3 updated

--- PB#3/pbnum3.py
+++ PB#3/pbnum3.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 #Problem 1
 g0, gp = 2, 1
-T = 80 + 273.15
-dE = 1.06
-R = 1.987 * 10**-3
+T = 80 + 273.15 #K
+dE = 1.06 #kcal/mol
+R = 1.987 * 10**-3 #kcal/mol*K
 
 #n0/nt = g0 * exp(-E0/RT)/ exp(-E0/RT + -Ep/RT)
 #np/nt = gp * exp(-Ep/RT)/ exp(-E0/RT + -Ep/RT)
@@ -20,16 +20,16 @@ print(f'The ratio is {ratio:.4f}')
 xp = 1/ (1 + ratio)
 x0 = 1 - xp
 
-print(f'The ortho is {x0:.4f} and para is {xp:.4f}')
+print(f'The molar fractions of o-nitrophenol (o): {x0:.4f} and p-nitrophenol (p): {xp:.4f}')
 
 #Problem 2
 def distr(v, T, m):
     return (m * v / (kb * T)) * np.exp(-(m * v**2) / (2 * kb * T))
 
-m = 1.66 * 10**(-27)
-Rg =  8.3145
-v = np.linspace(0, 7500, 500)
-kb =  1.3803 * 10**(-23)
+m = 1.66 * 10**(-27) #kg
+Rg =  8.3145 #j/mol*K
+v = np.linspace(0, 7500, 500) #m/s
+kb =  1.3803 * 10**(-23) #J/K
 
 plt.plot(v, distr(v, 200, m), color='red', label='Distribution plot for 200K')
 plt.plot(v, distr(v, 300, m), color='black', label='Distribution plot for 300K')
